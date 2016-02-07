971fb5d6a775dd3d82708040e86c6bb048d55243
Anant Singh <anantsingh1010@gmail.com>
2016-02-07 13:06:13 +0530

    Update weeklytop.py

--- weeklytop.py
+++ weeklytop.py
@@ -9,6 +9,6 @@ link = soup.find_all("li",{"class":"song-wrap"})
 for item in link :
 	print item.find_all("div",{"class":"index"})[0].text +" -> "+item.find_all("span",{"class":"title"})[0].text
 	print "Album -> "+item.find_all("a",{"class":"meta-album"})[0].text
-	print "Artist -> "+item.find_all("a",{"class":"multi-meta"})[0v].text
+	print "Artist -> "+item.find_all("a",{"class":"multi-meta"})[0].text
 	print "Year -> "+item.find_all("a",{"class":"meta-year"})[0].text
 	print '\n \n'
